4222e8cfc70aefc38f87cc46b6513f8a63535cb1
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-09-17 13:24:53 -0400

    Executing the script properly

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -ex
 
 sudo apt-get update && sudo apt-get upgrade -y
 
-curl -O -L -C - https://www.vmware.com/go/getWorkstation-linux -o ~/getWorkstation-linux
-chmod +x ~/getWorkstation-linux
-~/getWorkstation-linux -d
+curl -O -L -C - https://www.vmware.com/go/getWorkstation-linux
+chmod +x ./getWorkstation-linux
+./getWorkstation-linux -d
 
 sudo find / -name vmware
