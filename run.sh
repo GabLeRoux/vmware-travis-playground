2c047a2d892ea8b6745931b394fb9d2979886c58
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-09-17 13:37:44 -0400

    Fix arguments

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 curl -O -L -C - https://www.vmware.com/go/getWorkstation-linux
 chmod +x ./getWorkstation-linux
-./getWorkstation-linux -d
+./getWorkstation-linux
 
 sudo find / -name vmware
